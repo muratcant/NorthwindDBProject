233eb30bfe7b54a68942e2b8ad067a23b9882391
muratcant <muratcantanriverdi96@gmail.com>
2021-05-15 14:00:37 +0300

    Veri erişim katmanı kodlandı.

---
+++ NorthwindDBProject.DataAccess/Concrete/EntityFramework/EFCustomerDal.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindDBProject.Core.DataAccess.EntityFramework;
+using NorthwindDBProject.DataAccess.Abstract;
+using NorthwindDBProject.Entities.Entities;
+
+namespace NorthwindDBProject.DataAccess.Concrete.EntityFramework
+{
+    public class EFCustomerDal : EFRepository<Customer, NorthwindDBContext>, ICustomerDal
+    {
+    }
+}
---
+++ NorthwindDBProject.DataAccess/Concrete/EntityFramework/EFSupplierDal.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindDBProject.Core.DataAccess.EntityFramework;
+using NorthwindDBProject.DataAccess.Abstract;
+using NorthwindDBProject.Entities.Entities;
+
+namespace NorthwindDBProject.DataAccess.Concrete.EntityFramework
+{
+    public class EFSupplierDal : EFRepository<Supplier, NorthwindDBContext>, ISupplierDal
+    {
+    }
+}
